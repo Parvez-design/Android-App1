6d85d2605b35e030847b2240aa410737978336ce
Parvez <shpaah522@gmail.com>
2020-05-18 19:59:58 +0530

    layout edited

--- app/src/main/java/com/example/shpaahproject/MainActivity.kt
+++ app/src/main/java/com/example/shpaahproject/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.shpaahproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import com.example.shpaahproject.R.id.toolbar
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar?
+        setSupportActionBar(toolbar)
     }
+
 }
